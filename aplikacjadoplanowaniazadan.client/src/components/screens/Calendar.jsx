b0960db52c6af723b73ec1806c4e13a8c0827b7c
Krzysztof Mrugała <pankrzysztof20@gmail.com>
2024-11-10 16:03:27 +0100

    SCRUM-35 Dodanie liczników zadań przy dniach w kalendarzu i inne porawki.

--- aplikacjadoplanowaniazadan.client/src/components/screens/Calendar.jsx
+++ aplikacjadoplanowaniazadan.client/src/components/screens/Calendar.jsx
@@ -10,20 +10,73 @@ import Divider from '@mui/material/Divider';
 import { Paper, LinearProgress } from '@mui/material';
 import Task from '../Task';
 import TaskListSkeleton from '../skeletons/TaskListSkeleton';
+import { DayCalendarSkeleton } from '@mui/x-date-pickers/DayCalendarSkeleton';
+import Badge from '@mui/material/Badge';
+import { PickersDay } from '@mui/x-date-pickers/PickersDay';
+
+function DayWithBadge(props) {
+    const { monthTaskCounts, day, outsideCurrentMonth, ...other } = props;
+
+    const generatedDay = ''+props?.day?.date();
+    const days = Object.keys(monthTaskCounts);
+
+    const isSelected =
+        !props.outsideCurrentMonth
+        && days?.find(day => day === generatedDay) > 0;
+
+    return (
+        <Badge
+            key={props.day.toString()}
+            overlap="circular"
+            badgeContent={isSelected ? monthTaskCounts[generatedDay] : undefined}
+            color="secondary"
+            sx={{ "& .MuiBadge-badge": { fontSize: 10, height: 16, minWidth: 16 } }}
+        >
+            <PickersDay {...other} outsideCurrentMonth={outsideCurrentMonth} day={day} />
+        </Badge>
+    );
+}
 
 export default function Calendar({ hidden }) {
+
+    const getMonthTaskCounts = async (date) => {
+        setLoadingCalendar(true);
+        try {
+            // wysy³a np 2024-11 i 30.
+            //console.log(date?.format('YYYY-MM'), date?.daysInMonth());
+            // otrzymuje np {13: 2, 16: 3}, gdzie {numerDnia: liczbaZadan}
+            // nie trzeba dawaæ dni bez zadañ
+            //setMonthTaskCounts({13: 2, 16: 3});
+
+            const response = await fetch("http://localhost:5141/api/list/getMonthTaskCounts", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(date?.format('YYYY-MM'), date?.daysInMonth()),
+            });
+            if (!response.ok)
+                throw Error(response?.status);
+            const data = await response.json();
+            setMonthTaskCounts(data);
+        }
+        catch (error) { }
+        finally {
+            setLoadingCalendar(false);
+        }
+    }
 
     const getDayTasks = async (date) => {
         setLoadingTasks(true);
         try {
             // wysy³ane do getDayTasks
-            console.log("getDayTasks: " + date?.format('YYYY-MM-DD'));
+            //console.log("getDayTasks: " + date?.format('YYYY-MM-DD'));
             const response = await fetch("http://localhost:5141/api/list/getDayTasks", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify(date?.toJSON()),
+                body: JSON.stringify(date?.format('YYYY-MM-DD')),
             });
             if (!response.ok)
                 throw Error(response?.status);
@@ -79,15 +132,27 @@ export default function Calendar({ hidden }) {
         }
     }
 
+    const [loadingCalendar, setLoadingCalendar] = React.useState(false);
     const [loadingTasks, setLoadingTasks] = React.useState(false);
     const [loadingTaskAction, setLoadingTaskAction] = React.useState([]);
     const [selectedDate, setSelectedDate] = React.useState(dayjs());
-    const [dayTasks, setDayTasks] = React.useState(null);
+    const [monthTaskCounts, setMonthTaskCounts] = React.useState({});
+    const [dayTasks, setDayTasks] = React.useState([]);
 
     React.useEffect(() => {
         if (!hidden)
             getDayTasks(selectedDate);
     }, [hidden, selectedDate]);
+
+    React.useEffect(() => {
+        if (!hidden)
+            getMonthTaskCounts(dayjs());
+    }, [hidden]);
+
+    const handleMonthChange = (date) => {
+        setMonthTaskCounts({});
+        getMonthTaskCounts(date);
+    };
 
     return (
         <Box 
@@ -100,8 +165,21 @@ export default function Calendar({ hidden }) {
         >
             <LocalizationProvider dateAdapter={AdapterDayjs}>
                 <DateCalendar
+                    showDaysOutsideCurrentMonth
+                    fixedWeekNumber={6}
                     value={selectedDate}
                     onChange={(newValue) => setSelectedDate(newValue)}
+                    onMonthChange={handleMonthChange}
+                    loading={loadingCalendar}
+                    renderLoading={() => <DayCalendarSkeleton />}
+                    slots={{
+                        day: DayWithBadge,
+                    }}
+                    slotProps={{
+                        day: {
+                            monthTaskCounts,
+                        }
+                    }}
                 />
             </LocalizationProvider>
             <Divider variant='middle' flexItem />
