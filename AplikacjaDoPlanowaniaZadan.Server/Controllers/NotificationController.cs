7a6b5429881b428464935b478a1507d2e4f472e2
Paweł Żuberek <pawelpno@gmail.com>
2024-12-09 01:10:21 +0100

    SCRUM-48

---
+++ AplikacjaDoPlanowaniaZadan.Server/Controllers/NotificationController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using AplikacjaDoPlanowaniaZadan.Server.DataModels;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.Net.Http.Headers;
+using Microsoft.EntityFrameworkCore;
+using System;
+using AplikacjaDoPlanowaniaZadan.Server.DAL.EF;
+
+namespace AplikacjaDoPlanowaniaZadan.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NotificationController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NotificationController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("getNotifications")]
+        public IActionResult GetNotifications()
+        {
+            var token = Request.Headers[HeaderNames.Authorization].FirstOrDefault()?.Split(" ").Last();
+            if (string.IsNullOrEmpty(token))
+            {
+                return Unauthorized("Token is missing.");
+            }
+
+            var handler = new JwtSecurityTokenHandler();
+            var decodedToken = handler.ReadJwtToken(token);
+            var email = decodedToken.Claims.FirstOrDefault(claim => claim.Type == "email")?.Value;
+
+            if (string.IsNullOrEmpty(email))
+            {
+                return Unauthorized("Invalid token.");
+            }
+
+            var user = _context.Users
+                .Where(u => u.Email == email)
+                .Include(u => u.Notifications) 
+                .FirstOrDefault();
+
+            if (user == null)
+            {
+                return BadRequest("User not found.");
+            }
+
+            var notifications = user.Notifications
+                .OrderByDescending(n => n.SendDate)
+                .ToList();
+
+            return Ok(notifications);
+        }
+    }
+}
--- AplikacjaDoPlanowaniaZadan.Server/DAL/EF/ApplicationDbContext.cs
+++ AplikacjaDoPlanowaniaZadan.Server/DAL/EF/ApplicationDbContext.cs
@@ -11,7 +11,9 @@ namespace AplikacjaDoPlanowaniaZadan.Server.DAL.EF
         public DbSet<DataModels.Task> Tasks { get; set; }
         public DbSet<DataModels.List> Lists { get; set; }
         public DbSet<DataModels.User> Users { get; set; }
+        public DbSet<DataModels.Notification> Notifications { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
